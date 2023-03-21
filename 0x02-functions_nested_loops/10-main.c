ff67a5fd837d1a5d2e83763fe34f8835c4fddbcc
Haronlovy <haronkiplimo10@gmail.com>
2023-03-21 04:22:57 -0700

    Update

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int a;
+    int b;
+    int n;
+
+    n = a + b;
+    printf("%d\n", n);
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Prints all natural number from n to 98,
+ * followed by a new line
+ * @num: start number
+ * Return: Always void
+*/
+
+void print_to_98(int num)
+{
+	if (num > 98)
+	{
+		for (; num >= 98; num--)
+		{
+			printf("%d", num);
+			if (num != 98)
+			{
+				printf("%s", ", ");
+			}
+		}
+		putchar('\n');
+	}
+	else
+	{
+		for (; num <= 98; num++)
+		{
+			printf("%d", num);
+			if (num != 98)
+			{
+				printf("%s", ", ");
+			}
+		}
+		putchar('\n');
+	}
+}
