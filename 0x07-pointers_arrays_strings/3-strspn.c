dca917b0237c6b02886bd73cd649c3967e581936
Haronlovy <haronkiplimo10@gmail.com>
2023-04-03 03:01:09 -0700

    pointers arrays tasks

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _strspn - Entry point
- * input
- * result: input
+ * @s: input
+ * @accept: input
  * Return: Always 0 (Success)
  */
 unsigned int _strspn(char *s, char *accept)
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _strpbrk - Entry point
- * input value
- * @result: input
+ * @s: input
+ * @accept: input
  * Return: Always 0 (Success)
  */
 char *_strpbrk(char *s, char *accept)
